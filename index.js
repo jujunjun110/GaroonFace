d2f1a2b7ca4183ecbe9facfea8569ee02ab95ac4
Jun Ito <jun_ito@voyagegroup.com>
2017-02-06 18:50:19 +0900

    Update: さすがにセミコロンつけた

--- index.js
+++ index.js
@@ -1,44 +1,43 @@
-var s = document.createElement("script")
-s.src = "//cdnjs.cloudflare.com/ajax/libs/superagent/3.4.2/superagent.min.js"
+var s = document.createElement("script");
+s.src = "//cdnjs.cloudflare.com/ajax/libs/superagent/3.4.2/superagent.min.js";
 s.onload = function () {
-    var request = window.superagent
-    var names = document.querySelectorAll('.username_grn')
+    var request = window.superagent;
+    var names = document.querySelectorAll('.username_grn');
     names.forEach(function (item) {
-        var href = item.childNodes[0].getAttribute("href")
+        var href = item.childNodes[0].getAttribute("href");
         if (!href) {
-            return
+            return;
         }
-        var link = href.split("'")[1]
-        var imgPageUrl = 'https://' + document.location.hostname + link
+        var link = href.split("'")[1];
+        var imgPageUrl = 'https://' + document.location.hostname + link;
 
         var localImageUrl = localStorage.getItem(imgPageUrl);
         if (localImageUrl) {
-            setImage(item, localImageUrl)
-            return
+            setImage(item, localImageUrl);
+            return;
         }
 
-        console.log("request: " + imgPageUrl)
+        console.log("request: " + imgPageUrl);
 
         request
             .get(imgPageUrl)
             .end(function (err, res) {
-                var el = document.createElement('html')
+                var el = document.createElement('html');
                 el.innerHTML = res.text;
-                var imgs = el.getElementsByTagName("img")
-                var imgPath = imgs[1].getAttribute("src")
-                setImage(item, imgPath)
-                localStorage.setItem(imgPageUrl, imgPath)
+                var imgs = el.getElementsByTagName("img");
+                var imgPath = imgs[1].getAttribute("src");
+                setImage(item, imgPath);
+                localStorage.setItem(imgPageUrl, imgPath);
             });
     })
 
     function setImage(item, imgPath) {
-        var imgEl = document.createElement("img")
-        imgEl.setAttribute("src", imgPath)
+        var imgEl = document.createElement("img");
+        imgEl.setAttribute("src", imgPath);
         imgEl.setAttribute("style", 'display: block; width: 80px');
-        item.parentNode.appendChild(imgEl)
+        item.parentNode.appendChild(imgEl);
     }
 };
 
 document.body.appendChild(s);
-
 
