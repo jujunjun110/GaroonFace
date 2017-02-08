d4e7f4c83515cc99d887c8f11e5d26fe9a2ced1f
Jun Ito <jun_ito@voyagegroup.com>
2017-02-08 14:32:41 +0900

    Update: りふぁくた〜

--- index.js
+++ index.js
@@ -1,47 +1,50 @@
-var s = document.createElement("script");
-s.src = "//cdnjs.cloudflare.com/ajax/libs/superagent/3.4.2/superagent.min.js";
+(function () {
+    var s = document.createElement("script");
+    s.src = "//cdnjs.cloudflare.com/ajax/libs/superagent/3.4.2/superagent.min.js";
 
-s.onload = function () {
-    var request = window.superagent;
-    var nameElements = document.querySelectorAll('.username_grn');
-    nameElements.forEach(function (item) {
-        var href = item.querySelectorAll("a")[0].getAttribute('href');
+    s.onload = function () {
+        var request = window.superagent;
+        var nameElements = document.querySelectorAll('.username_grn');
+        nameElements.forEach(function (item) {
+            var anchor = item.querySelector('a');
 
-        if (!href) {
-            return;
+            if (!anchor) {
+                return;
+            }
+
+            var path = anchor.getAttribute('href').split("'")[1];
+            var imgPageUrl = document.location.origin + path;
+
+            var localImageUrl = localStorage.getItem(imgPageUrl);
+            if (localImageUrl) {
+                setImage(item, localImageUrl);
+                return;
+            }
+
+            console.log("request: " + imgPageUrl);
+
+            request
+                .get(imgPageUrl)
+                .end(function (err, res) {
+                    var el = document.createElement('html');
+                    el.innerHTML = res.text;
+                    var imgs = el.getElementsByTagName("img");
+                    var imgPath = imgs[1].getAttribute("src");
+                    setImage(item, imgPath);
+                    localStorage.setItem(imgPageUrl, imgPath);
+                });
+        })
+
+        function setImage(item, imgPath) {
+            var imgEl = document.createElement("img");
+            imgEl.setAttribute("src", imgPath);
+            imgEl.setAttribute("style", 'display: block; width: 80px');
+            item.parentNode.appendChild(imgEl);
         }
 
-        var path = href.split("'")[1];
-        var imgPageUrl = document.location.origin + path;
+    };
 
-        var localImageUrl = localStorage.getItem(imgPageUrl);
-        if (localImageUrl) {
-            setImage(item, localImageUrl);
-            return;
-        }
+    document.body.appendChild(s);
 
-        console.log("request: " + imgPageUrl);
-
-        request
-            .get(imgPageUrl)
-            .end(function (err, res) {
-                var el = document.createElement('html');
-                el.innerHTML = res.text;
-                var imgs = el.getElementsByTagName("img");
-                var imgPath = imgs[1].getAttribute("src");
-                setImage(item, imgPath);
-                localStorage.setItem(imgPageUrl, imgPath);
-            });
-    })
-
-    function setImage(item, imgPath) {
-        var imgEl = document.createElement("img");
-        imgEl.setAttribute("src", imgPath);
-        imgEl.setAttribute("style", 'display: block; width: 80px');
-        item.parentNode.appendChild(imgEl);
-    }
-
-};
-
-document.body.appendChild(s);
+}())
 
